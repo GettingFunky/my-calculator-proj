e79743fff2c83840e367a3b8b81c5c0488a5ad1f
Ev. Theodorakis <sagemmyV75@yahoo.gr>
2024-12-17 00:02:40 +0200

    hotfix

--- src/gr/aueb/cf/ch2/SubCalculator.java
+++ src/gr/aueb/cf/ch2/SubCalculator.java
@@ -9,6 +9,7 @@ public class SubCalculator {
 
         result = sub(num1, num2);
 
+        System.out.println("Sub: " + result);
     }
 
     public static int sub(int a, int b) {
@@ -16,4 +17,4 @@ public class SubCalculator {
     }
 }
 
-}
+
